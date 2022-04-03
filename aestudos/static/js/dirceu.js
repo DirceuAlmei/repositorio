4f37efac77c9dfdceaf37590ac29cf9060223fb1
Dirceu Aguiar <78818471+DirceuAlmei@users.noreply.github.com>
2022-04-03 16:58:53 -0300

    Elaboração do header

--- aestudos/static/js/dirceu.js
+++ aestudos/static/js/dirceu.js
@@ -1,1 +1,16 @@
 console.log('Tudo dentro dos conformes...!!!')
+
+var btnMobile = document.querySelector('.btn-mobile');
+var hamburGue = document.querySelector('.hamburguer');
+
+btnMobile.addEventListener("click", function(){
+    document.querySelector(".sidebar").classList.toggle("ativar");
+    document.querySelector(".hamburguer").classList.toggle("open");
+})
+
+var dirceu = document.getElementById('dirceu');
+function dirceuaguiar(){
+    var ilma = document.getElementById('iiiii')
+    ilma.classList.toggle('click')
+}
+dirceu.addEventListener('click', dirceuaguiar)
